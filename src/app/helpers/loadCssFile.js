e905bbda9adb3037a66a47fb0afde4e528b95a2d
munira-alduraibi <mkd1433@gmail.com>
2023-12-07 19:57:54 +0300

    add the icons

--- src/app/helpers/loadCssFile.js
+++ src/app/helpers/loadCssFile.js
@@ -1,6 +1,6 @@
 export default (pathName) => {
     const link = document.createElement("link")
-    link.href = `/css/${pathName}.css`;
+    link.href = `/portfolio/css/${pathName}.css`;
     link.rel = "stylesheet"
     
     document.head.appendChild(link)
